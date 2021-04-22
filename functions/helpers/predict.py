594121c4bfa6838a7bf8ed7f3063f6af682fb9a5
meomkarchavan <chavanomkar245@gmail.com>
2021-04-22 10:14:56 +0530

    test

--- functions/helpers/predict.py
+++ functions/helpers/predict.py
@@ -4,12 +4,10 @@ import io
 import os
 import pandas as pd
 import numpy as np
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 class Predict():
     def __init__(self,model_path):
-        config = tf.ConfigProto(
-        device_count = {'GPU': 0})
-        tf.Session(config=config)
         self.model=tf.keras.models.load_model(model_path)
     def predict(self,df):
         X = df.drop(['AREA','REGION'], axis = 1).astype('float32')
